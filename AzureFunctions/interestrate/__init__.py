259e70c7f88bbcd2d82bd593aa5a0f48b262960a
MichalMoravik <michal.moravik@icloud.com>
2020-11-22 23:25:55 +0100

    bank - list_deposits() and create_loan() added. New serverless function 'loanalgorithm' added

--- AzureFunctions/interestrate/__init__.py
+++ AzureFunctions/interestrate/__init__.py
@@ -4,12 +4,11 @@ import json
 
 def main(req: func.HttpRequest) -> func.HttpResponse:  
     try: 
-        req_body = req.get_json()
+        deposited_amount = req.get_json().get('amount')
     except ValueError as e:
-        return func.HttpResponse(body=f"*** Could not get body of the request! *** \n{e}", status_code=404)
+        return func.HttpResponse(body=f"*** Body of the request is not valid! *** \n{e}", status_code=404)
     else:
-        deposited_amount = req_body.get('amount')
         interest_rate = 0.02
         # returned amount is a deposited amount + 2% (interest rate) from the deposited amount
         new_amount = deposited_amount + deposited_amount * interest_rate
-        return func.HttpResponse(body=json.dumps({"amountWithInterest": new_amount}))
+        return func.HttpResponse(body=json.dumps({"amountWithInterest": new_amount}), status_code=200)
--- Bank/nemidbank/routes/general.py
+++ Bank/nemidbank/routes/general.py
@@ -60,6 +60,87 @@ def add_deposit():
             return jsonify("Server error: Could not successfully perform this operation. " \
                 "Possible database problem"), 500
         else:
-            return jsonify({"msg": "Operation successfully performed and stored!", 
+            return jsonify({"msg": "Operation successfully performed and recorded!", 
                             "newAmount": str(new_amount)}), 201
 
+
+@app.route('/list-deposits/<id>', methods=['GET'])
+def list_deposits(id):
+    try:
+        bank_user_id = int(id)
+    except Exception as e:
+        print(f"*** Error in routes/general/list_deposits() *** \n{e}")
+        return jsonify("Check spelling and data types of request body elements!"), 400
+    else:
+        try:
+            cur = get_db().cursor()
+            cur.execute("SELECT * FROM Deposit WHERE BankUserId=?", (bank_user_id,))
+            rows = cur.fetchall()
+        except Exception as e:
+            print(f"*** Error in routes/general/list_deposits() ***: \n{e}")
+            return jsonify("Server error: Cannot retrieve deposits!"), 500
+        else:
+            if rows:
+                deposits = [dict(deposit) for deposit in rows]
+                return json.dumps(deposits), 200
+            return jsonify(f'There are no deposits from the bank user with this id: {bank_user_id}!'), 404
+        
+        
+@app.route('/create-loan', methods=['POST'])
+def create_loan():
+    try:
+        loan_amount = int(request.json['loanAmount'])
+        bank_user_id = int(request.json['bankUserId'])
+        created_at = datetime.now().strftime("%B %d, %Y %I:%M%p")
+        modified_at = datetime.now().strftime("%B %d, %Y %I:%M%p")
+    except Exception as e:
+        print(f"*** Error in routes/general/create_loan() *** \n{e}")
+        return jsonify("Check spelling and data types of request body elements!"), 400
+    else:
+        try:
+            cur = get_db().cursor()
+                
+            # find out if the specified account exists and obtain current amount
+            cur.execute(f"SELECT Amount FROM Account WHERE BankUserId=?", (bank_user_id,))
+            record = cur.fetchone()
+            if record is None:
+                return jsonify(f'A bank user with id: {bank_user_id} does not have a bank account!'), 404
+            old_amount = dict(record)['Amount']
+            
+            try:
+                # send the loan amount to the loan algorithm function
+                response = requests.post('http://localhost:7071/api/loanalgorithm', 
+                                        json={
+                                            "amount": loan_amount,
+                                            "accountAmount": old_amount
+                                            })
+            except Exception as e:
+                print(f"*** Error in routes/general/create_loan() *** \n{e}")
+                return jsonify("Server error: Could not receive a status code " \
+                    "from the 'loanalgorithm' serverless function!"), 500
+            else:
+                if response.status_code != 200:
+                    return jsonify("Error: The loan amount cannot be higher than 75% " \
+                        "of current holdings!"), 403
+                
+                # new amount is the old (current) amount of money on the account plus the loan
+                new_amount = old_amount + loan_amount
+                    
+                # transaction - update account with a new amount and insert into loan table a new record
+                commands = [
+                    ('UPDATE Account SET ModifiedAt=?, Amount=? WHERE BankUserId=?', 
+                        (modified_at, new_amount, bank_user_id)),
+                    ('INSERT INTO Loan(BankUserId, CreatedAt, ModifiedAt, Amount) VALUES (?,?,?)', 
+                        (bank_user_id, created_at, modified_at, loan_amount))]
+                
+                for command in commands:
+                    cur.execute(command[0], command[1])
+                    
+                get_db().commit()
+        except Exception as es:
+            print(f"*** Error in routes/general/create_loan() *** \n{es}")
+            return jsonify("Server error: Could not successfully perform this operation. " \
+                "Possible database problem"), 500
+        else:
+            return jsonify({"msg": "Operation successfully performed and recorded!", 
+                            "newAmount": str(new_amount)}), 201
